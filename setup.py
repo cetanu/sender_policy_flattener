f7745c163cff03425b64a432d2e82b4090cdabc9
Basileus <cetanu@gmail.com>
2022-01-21 17:59:31 +1100

    increment patch

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name='sender_policy_flattener',
-    version='0.2.4',
+    version='0.2.5',
     packages=['sender_policy_flattener'],
     long_description=readme(),
     long_description_content_type='text/markdown',
