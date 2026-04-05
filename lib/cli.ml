131b83c807bf309733d1c36bd294face1fdb8409
Vasilios Syrakis <cetanu@gmail.com>
2026-04-05 21:57:08 +1000

    Trying to query DNS and print results

--- lib/cli.ml
+++ lib/cli.ml
@@ -1,13 +1,30 @@
 open Cmdliner
 
+
+let lookup client rr_name rr_type = 
+    Printf.printf "  %s %s -- " rr_name (Cfg.string_of_rrtype rr_type);
+
+    let rr = match rr_type with
+        | Cfg.A -> Dns.Rr_map.A
+        | Cfg.AAAA -> Dns.Rr_map.Aaaa
+        | Cfg.CNAME -> Dns.Rr_map.Cname
+        | Cfg.MX -> Dns.Rr_map.Mx
+        | Cfg.TXT -> Dns.Rr_map.Txt
+        | s -> Dns.Rr_map.A
+    in
+    let _result = Dns_client_unix.get_resource_record client rr rr_name 
+    Printf.printf "Result: %s\n" _result in
+    ()
+
 let compress name =
     let config = Cfg.load_config name in
-    Hashtbl.iter (fun domain inner ->
-      Printf.printf "\n[%s]\n" domain;
-      Hashtbl.iter (fun host rr ->
-        Printf.printf "  %s %s\n" host (Cfg.string_of_rrtype rr)
-      ) inner
+    let client = Dns_client_unix.create () in
+    Hashtbl.iter (fun sending_domain inner ->
+      Printf.printf "\n[%s]\n" sending_domain;
+      Hashtbl.iter (lookup client) inner
     ) config
+
+
 
 let config_arg =
   let doc = "Location of JSON config file on disk" in
